9ee12ca511dde0084d07a3da25772197fb40775d
Marko Uljarevic <marko.uljarevic@procescom.com>
2018-11-14 15:42:02 +0100

    add mailer and new dependency

--- src/main/java/com/webapp/lora/LoraApplication.java
+++ src/main/java/com/webapp/lora/LoraApplication.java
@@ -1,12 +1,35 @@
 package com.webapp.lora;
 
+
+
+import com.webapp.lora.util.Mailer;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class LoraApplication {
 
 	public static void main(String[] args) {
+		Logger log = Logger.getLogger(LoraApplication.class);
+	//	log.error("TEST *******");
+		log.warn("TEST *******");
+		log.trace("TEST *******");
+		log.warn("TEST *******");
+		log.trace("TEST *******");
+		log.warn("TEST *******");
+		log.trace("TEST *******");log.warn("TEST *******");
+		log.trace("TEST *******");
+		log.warn("TEST *******");
+		log.trace("TEST *******");
+
+		System.out.println("TESTI --------------------------");
 		SpringApplication.run(LoraApplication.class, args);
+	//	log.error("TEST *******");
+		log.warn("TEST *******");
+		log.trace("TEST *******");
+		System.out.println("TESTI --------------------------");
+		new Mailer().logMail("This mail should be sent");
 	}
 }
