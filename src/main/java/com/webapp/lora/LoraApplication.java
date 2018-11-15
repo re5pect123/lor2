fbd8bcd58dd6a692d7e38c89b646b3c60291746a
Marko Uljarevic <marko.uljarevic@procescom.com>
2018-11-15 13:12:17 +0100

    add all desc and examples for log4j

--- src/main/java/com/webapp/lora/LoraApplication.java
+++ src/main/java/com/webapp/lora/LoraApplication.java
@@ -11,25 +11,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoraApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Logger log = Logger.getLogger(LoraApplication.class);
-	//	log.error("TEST *******");
-		log.warn("TEST *******");
-		log.trace("TEST *******");
-		log.warn("TEST *******");
-		log.trace("TEST *******");
-		log.warn("TEST *******");
-		log.trace("TEST *******");log.warn("TEST *******");
-		log.trace("TEST *******");
-		log.warn("TEST *******");
-		log.trace("TEST *******");
 
-		System.out.println("TESTI --------------------------");
 		SpringApplication.run(LoraApplication.class, args);
-	//	log.error("TEST *******");
-		log.warn("TEST *******");
-		log.trace("TEST *******");
-		System.out.println("TESTI --------------------------");
-		new Mailer().logMail("This mail should be sent");
+
+		for (;;){
+			log.info("test");
+			Thread.sleep(5000);
+		}
+
 	}
 }
