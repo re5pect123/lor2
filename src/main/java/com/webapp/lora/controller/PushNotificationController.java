c7976ea2c5c7aa9c7841b4b6801f2b1864e360e9
Marko Uljarevic <marko.uljarevic@procescom.com>
2019-01-08 15:05:35 +0100

    set notification for firebase

---
+++ src/main/java/com/webapp/lora/controller/PushNotificationController.java
@@ -0,0 +1,63 @@
+package com.webapp.lora.controller;
+
+import com.webapp.lora.model.NotificationData;
+import com.webapp.lora.model.NotificationRequestModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+@RestController
+public class PushNotificationController {
+
+    @GetMapping("/test-notification")
+    public void test() throws IOException {
+        System.out.println("Welcome to Developine");
+
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        HttpPost postRequest = new HttpPost(
+                "https://fcm.googleapis.com/fcm/send");
+
+        NotificationRequestModel notificationRequestModel = new NotificationRequestModel();
+        NotificationData notificationData = new NotificationData();
+
+        notificationData.setDetail("Detail");
+        notificationData.setTitle("title");
+        notificationRequestModel.setData(notificationData);
+        notificationRequestModel.setTo("/topics/all");
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<NotificationRequestModel>() {
+        }.getType();
+
+        String json = gson.toJson(notificationRequestModel, type);
+
+        StringEntity input = new StringEntity(json);
+        input.setContentType("application/json");
+
+        postRequest.addHeader("Authorization", "key=AAAA0_QWZXc:APA91bEzNN4vmGlwhKnIbulLa7YoLbkb1Gq9rz3XiGBIUgcsM4COD8WkmLbvt0eRo5eko51nMquC5kq0RaQ1bmaRIra1-AW0uObJegvKeHt-XLd9PtMPLSBEx5_bIvP-j5Zt5iel9iqL");
+        postRequest.setEntity(input);
+
+        System.out.println("request:" + json);
+
+        HttpResponse response = httpClient.execute(postRequest);
+
+        if (response.getStatusLine().getStatusCode() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + response.getStatusLine().getStatusCode());
+        } else if (response.getStatusLine().getStatusCode() == 200) {
+
+            System.out.println("response:" + EntityUtils.toString(response.getEntity()));
+
+        }
+    }
+}
